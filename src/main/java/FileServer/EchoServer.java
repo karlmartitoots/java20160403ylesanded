8f39dd9dd77bd18d7705c80db4a7305b7204869b
karlmartitoots <karlmarti65@gmail.com>
2016-04-08 22:43:37 +0300

    server now closes after EOFexcption

--- src/main/java/FileServer/EchoServer.java
+++ src/main/java/FileServer/EchoServer.java
@@ -2,6 +2,7 @@ package FileServer;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -11,30 +12,31 @@ public class EchoServer {
     public static void main(String[] args) throws IOException {
 
         ServerSocket serverSocket = new ServerSocket(1337);
-
-        while (true) {
-            Socket incoming = serverSocket.accept();
-            new Thread(() -> {
-                try (
-                        DataOutputStream output = new DataOutputStream(incoming.getOutputStream());
-                        DataInputStream input = new DataInputStream(incoming.getInputStream())) {
-                    while(true) {
-                        String info = "";
-                        info = input.readUTF();
-                        output.writeUTF(info);
-                    }
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } finally {
-                    try {
-                        incoming.close();
+        try {
+            while (true) {
+                Socket incoming = serverSocket.accept();
+                new Thread(() -> {
+                    try (
+                            DataOutputStream output = new DataOutputStream(incoming.getOutputStream());
+                            DataInputStream input = new DataInputStream(incoming.getInputStream())) {
+                        while (true) {
+                            String info = "";
+                            info = input.readUTF();
+                            output.writeUTF(info);
+                        }
                     } catch (IOException e) {
                         throw new RuntimeException(e);
+                    } finally {
+                        try {
+                            incoming.close();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
                     }
-                }
-            }).start();
+                }).start();
+            }
+        }catch(EOFException e){
+            serverSocket.close();
         }
-
     }
-
 }
