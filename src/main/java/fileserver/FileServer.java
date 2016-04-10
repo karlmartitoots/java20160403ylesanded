d1acc2f38d6111df6cae09a1017c422645f3176a
karlmartitoots <karlmarti65@gmail.com>
2016-04-11 00:15:24 +0300

    Cleaned up fileserver

--- src/main/java/fileserver/FileServer.java
+++ src/main/java/fileserver/FileServer.java
@@ -3,58 +3,71 @@ package fileserver;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
 
 public class FileServer {
 
     public static void main(String[] args) throws IOException {
 
-        ServerSocket serverSocket = new ServerSocket(1337);
-        try {
+        try (ServerSocket serverSocket = new ServerSocket(1337)) {
             while (true) {
-                Socket incoming = serverSocket.accept();
+                Socket openSocket = serverSocket.accept();
+
                 new Thread(() -> {
-                    try (
-                            DataInputStream input = new DataInputStream(incoming.getInputStream())) {
-                        String resourceName = input.readUTF();
-
-                        try(
-                                DataOutputStream output = new DataOutputStream(incoming.getOutputStream());
-                                InputStream resourceInputStream = FileServer.class.getClassLoader().getResourceAsStream(resourceName)){
-
-                            if(resourceInputStream == null){
-                                incoming.close();
-                                Thread.currentThread().interrupt();
-                                return;
-                            }
-
-                            int read;
-                            int fileSize = 0;
-                            byte[] byteChunk = new byte[1024];
-
-                            ByteArrayOutputStream deezBytes = new ByteArrayOutputStream();
-                            while((read = resourceInputStream.read(byteChunk, 0, 1024)) != -1){
-                                fileSize += read;
-                                deezBytes.write(byteChunk, 0, read);
-                            }
-
-                            output.writeInt(fileSize);
-                            deezBytes.writeTo(output);
-                        }
+                    try (DataInputStream dataInputStream = new DataInputStream(openSocket.getInputStream())) {
+                        String resourceName = dataInputStream.readUTF();
+                        sendByteArray(resourceName, openSocket);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     } finally {
                         try {
-                            incoming.close();
+                            openSocket.close();
                         } catch (IOException e) {
                             throw new RuntimeException(e);
                         }
                     }
                 }).start();
             }
-        }catch(EOFException e){
-            serverSocket.close();
         }
+    }
+
+    /**
+     * Sends a file to the client as a byte array using an open socket
+     * @param resourceName Name of the resource to be sent
+     * @param openSocket An open socket connected to the client
+     * @throws IOException
+     */
+    private static void sendByteArray(String resourceName, Socket openSocket) throws IOException {
+        try(
+                DataOutputStream dataOutputStream = new DataOutputStream(openSocket.getOutputStream());
+                InputStream resourceInputStream = FileServer.class.getClassLoader().getResourceAsStream(resourceName)){
+
+            if(resourceInputStream == null){
+                openSocket.close();
+                Thread.currentThread().interrupt();
+                return;
+            }
+
+            ByteArrayOutputStream byteArrayOutputStream = getResourceAsByteArrayOutputStream(resourceInputStream);
+
+            dataOutputStream.writeInt(byteArrayOutputStream.size());
+            byteArrayOutputStream.writeTo(dataOutputStream);
+        }
+    }
+
+    /**
+     * Turns a resourceInputStream into a ByteArrayOutputStream
+     * @param resourceInputStream Input stream for converting into byte array output stream
+     * @return Returns a stream that contains the resourceInputStream as a byte array
+     * @throws IOException
+     */
+    private static ByteArrayOutputStream getResourceAsByteArrayOutputStream(InputStream resourceInputStream) throws IOException {
+        int currentBufferSize;
+        byte[] byteChunk = new byte[1024];
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        while((currentBufferSize = resourceInputStream.read(byteChunk, 0, byteChunk.length)) != -1){
+            byteArrayOutputStream.write(byteChunk, 0, currentBufferSize);
+        }
+        return byteArrayOutputStream;
     }
 }
--- src/main/java/fileserver/FileServerClient.java
+++ src/main/java/fileserver/FileServerClient.java
@@ -8,22 +8,43 @@ public class FileServerClient {
 
     public static void main(String[] args) throws IOException {
 
-        try (
-                Socket socket = new Socket(InetAddress.getLocalHost(), 1337);
-                DataInputStream inputStream = new DataInputStream(socket.getInputStream());//input socketist(puhverdatud)
-                DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {//output socketile
-            outputStream.writeUTF(args[0]); //saadab failinime
-            File resource = new File(args[1]);
-            inputStream.readInt();//saab failisuuruse
+        try (Socket socket = new Socket(InetAddress.getLocalHost(), 1337);
+             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream())) {
 
-            int read;
-            byte[] byteChunk = new byte[1024];
-            try(
-                    BufferedOutputStream fileOutputStream = new BufferedOutputStream(new FileOutputStream(resource))) {
-                while ((read = inputStream.read(byteChunk, 0, byteChunk.length)) != -1) {
-                    fileOutputStream.write(byteChunk, 0, read);
-                }
+            if(args[0] == null){
+                System.out.println("1st commandline argument is null.");
+                System.exit(-1);
             }
+            outputStream.writeUTF(args[0]);
+
+            if (args[1] == null) {
+                System.out.println("2nd commandline argument is null.");
+                System.exit(-1);
+            }
+            File outputDirectory = new File(args[1]);
+
+            inputStream.readInt();
+
+            saveFile(inputStream, outputDirectory);
+        }
+    }
+
+    /**
+     * Saves all bytes from inputStream into outputDirectory
+     * @param inputStream Input stream of bytes
+     * @param outputDirectory Output directory for the file
+     * @throws IOException
+     */
+    private static void saveFile(DataInputStream inputStream, File outputDirectory) throws IOException {
+        int currentBufferSize;
+        byte[] byteChunk = new byte[1024];
+        try (BufferedOutputStream fileOutputStream = new BufferedOutputStream(new FileOutputStream(outputDirectory))) {
+
+            while ((currentBufferSize = inputStream.read(byteChunk, 0, byteChunk.length)) != -1) {
+                fileOutputStream.write(byteChunk, 0, currentBufferSize);
+            }
+
         }
     }
 
